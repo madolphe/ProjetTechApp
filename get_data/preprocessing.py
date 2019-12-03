5f60212a5054c2f00d32472a2d3365a2d524c2f0
madolphe <madolphe@ensc.fr>
2019-12-03 11:57:04 -0500

    feature exctraction finished
    finished frequency features + export new data_set to numpy files + install pandas + delete useless functions+pep8

--- get_data/preprocessing.py
+++ get_data/preprocessing.py
@@ -1,13 +1,13 @@
-# @TODO pre-processing pour extraction de characteristiques (à développer)
-
-import PIL
 from PIL import Image
 import numpy as np
+from numpy import savetxt
 import matplotlib.pyplot as plt
 from get_data.gather_data import *
 import cv2
 from skimage.feature import greycomatrix, greycoprops
 import pywt
+from pywt._doc_utils import wavedec2_keys, draw_2d_wp_basis
+
 
 def get_image(path, verbose=False):
     im = np.array(Image.open(path).resize((256, 256), Image.LANCZOS).convert("L"))
@@ -20,47 +20,10 @@ def get_image(path, verbose=False):
     return im
 
 
-def mean_image():
-    mean_img = np.mean(np.array([get_image(element) for element in x_train_pneumonia]), axis=0)
-    # plt.imshow(mean_img)
-    # plt.show()
-    return mean_img
-
-
-def histogram2d(mean_img, current_img):
-    hist, _, _ = np.histogram2d(mean_img.flatten(), current_img.flatten(), bins=100)
-    print(type(hist))
-    plt.imshow(hist)
-    plt.show()
-
-
-def sifting(img):
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(img,None)
-    img = cv2.drawKeypoints(img,kp,None)
-    cv2.imwrite('sift_keypoints.jpg', img)
-    cv2.imshow("h",img)
-
-
-def fourier(img):
-
-    transform_image = np.fft.rfft2(img)
-    shift = np.fft.fftshift(transform_image)
-    magnitude_spectrum = 20*np.log(np.abs(shift))
-    magnitude_spectrum = np.resize(magnitude_spectrum, (256, 256))
-
-    plt.subplot(121), plt.imshow(img, cmap='gray')
-    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
-    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-    plt.show()
-    return magnitude_spectrum
-
-
 def equal_hist(img, filter="median"):
     if filter == "median":
         img = cv2.medianBlur(img, ksize=3)
-    # Methode trouvée sur openCV
+    # Method found on opencv.org
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     cl1 = clahe.apply(img)
     return cl1
@@ -76,40 +39,32 @@ def features_texture_extraction(img):
     return features
 
 
-def features_complexity_extraction():
-    pass
-
-
-from pywt._doc_utils import wavedec2_keys, draw_2d_wp_basis
-
-
 def features_frequency_extraction(img):
-    # res = pywt.wavedec2(img, 'db1', 4)
-    # print(type(res))
-    # print(res[3])
-    # titles = ['Approximation', ' Horizontal detail',
-    #           'Vertical detail', 'Diagonal detail']
-    # coeffs2 = pywt.dwt2(img, 'bior1.3')
-    # LL, (LH, HL, HH) = coeffs2
-    # print(LL.shape)
-    # fig = plt.figure(figsize=(12, 3))
-    # for i, a in enumerate([LL, LH, HL, HH]):
-    #     ax = fig.add_subplot(1, 4, i + 1)
-    #     ax.imshow(a, interpolation="nearest", cmap=plt.cm.gray)
-    #     ax.set_title(titles[i], fontsize=10)
-    #     ax.set_xticks([])
-    #     ax.set_yticks([])
-    # fig.tight_layout()
-    # plt.show()
-    # On prendra moyenne et variance et ce sera all good
+    """
 
+    :param img:
+    :return:
+    """
+    shape = img.shape
+    max_lev = 4  # how many levels of decomposition
+    c = pywt.wavedec2(img, 'db2', mode='periodization', level=max_lev)
+    feature_vect = [np.mean(c[0]), np.std(c[0])]
+    # As wavedec2 returns coefficient in descending order, we don't look at last element:
+    for i in range(1, len(c)-1):
+        for elt in c[i]:
+            feature_vect.append(np.mean(elt))
+            feature_vect.append(np.std(elt))
+    return np.array(feature_vect)
+
+
+def dw_show(img):
     shape = img.shape
     max_lev = 3  # how many levels of decomposition to draw
     label_levels = 3  # how many levels to explicitly label on the plots
     fig, axes = plt.subplots(2, 4, figsize=[14, 8])
     for level in range(0, max_lev + 1):
         if level == 0:
-        # show the original image before decomposition
+            # show the original image before decomposition
             axes[0, 0].set_axis_off()
             axes[1, 0].imshow(img, cmap=plt.cm.gray)
             axes[1, 0].set_title('Image')
@@ -132,26 +87,45 @@ def features_frequency_extraction(img):
         axes[1, level].set_axis_off()
     plt.tight_layout()
     plt.show()
-    return
+
+
+def get_features(img, verbose=False):
+    if verbose:
+        # Faire un subplot
+        plt.imshow(equal_hist(img))
+        plt.title("Avec egalisation de l'histo")
+        plt.figure()
+        plt.imshow(img)
+        plt.title("Raw image")
+        plt.show()
+    img = equal_hist(img)
+    texture_features = features_texture_extraction(img)
+    freq_features = features_frequency_extraction(img)
+    features_vector = np.concatenate((texture_features, freq_features))
+    return features_vector
+
+
+def save_img_dataset_to_feature(path, name):
+    print(f"{name} en cours de traitement")
+    print(f"{len(path)} images to convert and save...")
+    data_set_features = []
+    for elt in path:
+        current = get_image(elt)
+        data_set_features.append(get_features(current))
+    path_to_save = os.path.join("..", "Data", "chest_xray_features_vect", name)
+    np.save(path_to_save, np.array(data_set_features))
 
 
 if __name__ == '__main__':
-    current = get_image(x_train_normal[100], verbose=False)
-    # texture_features = features_texture_extraction(current)
-    # features_frequency_extraction(current)
-    plt.imshow(equal_hist(current))
-    plt.title("Avec egalisation de l'histo")
-    plt.figure()
-    plt.imshow(current)
-    plt.title("Raw image")
-    plt.show()
-    # sifting(current)
-    # fou = fourier(current)
-    # mean = mean_image()
-    # histogram2d(mean, current)
 
+    def convert_all_datas():
+        save_img_dataset_to_feature(x_train_normal, "x_train_normal")
+        save_img_dataset_to_feature(x_test_normal, "x_test_normal")
+        save_img_dataset_to_feature(x_val_normal, "x_val_normal")
+        save_img_dataset_to_feature(x_train_pneumonia, "x_train_pneumonia")
+        save_img_dataset_to_feature(x_test_pneumonia, "x_test_pneumonia")
+        save_img_dataset_to_feature(x_val_pneumonia, "x_val_pneumonia")
 
-
-
+    convert_all_datas()
 
 
