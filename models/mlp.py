c6f9c12cb14d610ca52808f988af1e2083d7bac2
« <emmajouffroypro@gmail.com>
2019-12-03 14:11:01 -0500

    Init of mlp class, override of all functions in super class classifier, implementation of train function with MLPClassifier() object

--- models/mlp.py
+++ models/mlp.py
@@ -1,1 +1,51 @@
 #@TODO ajout classe 
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from .classifier import Classifier
+import sys
+from sklearn.neural_network import MLPClassifier
+
+
+class Mlp(Classifier):
+    def __init__(self, hyperparams):
+        super().__init__(hyperparams)
+        self.mlp = MLPClassifier() #best params
+
+    def set_mlp(self):
+        self.mlp = MLPClassifier() #new params after cross-val
+
+
+    def train(self, training_set, target_set):
+        """
+        """
+        print('multi-layer perceptron sklearn')
+        self.set_mlp()
+        # Récupérer les hyperparamètres depuis hyperparams après cross-validation
+        self.mlp.fit(training_set, target_set)
+
+    def predict(self):
+        """
+        """
+        pass
+
+    def error(self):
+        """
+        """
+        pass
+
+    def error_pred(self, training_set, target_set):
+        """
+
+        :param training_set:
+        :param target_set:
+        :return:
+        """
+        pass
+
+
+if __name__ == '__main__':
+    """
+    Liste de fonctions mises en place pour tester certaines partie de la classe présentée précédemment. 
+    # Non utile pour le bon fonctionnement du projet. 
+    """
