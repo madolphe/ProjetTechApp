066114be52d8f33e7d327fa8efa5df418c6db1c7
CyrilMougin <c.mougin@hotmail.fr>
2019-12-03 23:43:57 -0500

    Revert "Update svm.py"
    This reverts commit 62832fc0f6463c22243386a961cb240a978704ce.

--- models/svm.py
+++ models/svm.py
@@ -1,11 +1,1 @@
-#@TODO ajout classe
-
-from .classifier import Classifier
-
-
-class Svm(Classifier):
-
-    def __init__(self, hyperparams):
-        super().__init__(self, hyperparams)
-
-
+#@TODO ajout classe 
