62832fc0f6463c22243386a961cb240a978704ce
CyrilMougin <c.mougin@hotmail.fr>
2019-12-03 23:43:41 -0500

    Update svm.py

--- models/svm.py
+++ models/svm.py
@@ -1,1 +1,11 @@
-#@TODO ajout classe 
+#@TODO ajout classe
+
+from .classifier import Classifier
+
+
+class Svm(Classifier):
+
+    def __init__(self, hyperparams):
+        super().__init__(self, hyperparams)
+
+
