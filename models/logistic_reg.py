edc83067b3041872fdc3d7928b2767b056482261
madolphe <madolphe@ensc.fr>
2019-12-04 12:51:31 -0500

    init new feature branch logistic method
    change name file + init solution

---
+++ models/logistic_reg.py
@@ -0,0 +1,12 @@
+from models.classifier import Classifier
+
+
+class Logistic(Classifier):
+
+    def __init__(self, hyperparams):
+        super.__init__(hyperparams)
+        return
+
+    def train(self, training_set, target_set):
+        return
+
